3a018d4c5659de1d7aea43a12e8630d5b25deb3f
gauravsb14 <gauravsb14@gmail.com>
2018-04-10 13:01:06 +0530

    Docment similarity with all functions

--- Doc_Similarity_using_Gensim.py
+++ Doc_Similarity_using_Gensim.py
@@ -10,28 +10,28 @@ raw_documents = ["python developer with 3years experience.has to deep learning e
 
 gen_docs = [[w.lower() for w in word_tokenize(text)]
             for text in raw_documents]
-# print(gen_docs)
+print(gen_docs)
 
 dictionary = gensim.corpora.Dictionary(gen_docs)
-# print(dictionary)
+print(dictionary)
 # print(dictionary.token2id['road'])
-# print("Number of words in dictionary:",len(dictionary))
-# for i in range(len(dictionary)):
-#     print(i, dictionary[i])
+print("Number of words in dictionary:",len(dictionary))
+for i in range(len(dictionary)):
+    print(i, dictionary[i])
 #
 corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
 print(corpus)
 
 tf_idf = gensim.models.TfidfModel(corpus)
 print(tf_idf)
-# print(tf_idf[corpus[0]])
+print(tf_idf[corpus[0]])
 # s = 0
 # for i in corpus:
 #     s += len(i)
 # print(s)
 sims = gensim.similarities.Similarity('F:\FugenX_Project1',tf_idf[corpus],num_features=len(dictionary))
 print(sims)
-# print(type(sims))
+print(type(sims))
 #
 query_doc = [w.lower() for w in word_tokenize("python developer with 3years experience.")]
 print(query_doc)
@@ -42,6 +42,6 @@ print(query_doc_tf_idf)
 
 similarity_score = sims[query_doc_tf_idf]
 print(list(enumerate(similarity_score)))
-# print(similarity_score)
+print(similarity_score)
 #
 #
